5eacc231bc15b7ac974a5c6abd318bad3538f636
Emmanuel Joshua <emiranojoshua@gmail.com>
2026-04-21 17:45:59 +0100

    Implement CounterProvider and enhance HomePage with increment, decrement, and reset functionality

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,33 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    ChangeNotifierProvider(
+      child: const NewApp(),
+      create: (context) => CounterProvider(),
+    ),
+  );
+}
+
+class CounterProvider extends ChangeNotifier {
+  int _counter = 20;
+
+  int get counter => _counter;
+
+  void increment() {
+    _counter++;
+    notifyListeners();
+  }
+
+  void decrement() {
+    _counter--;
+    notifyListeners();
+  }
+
+  void reset() {
+    _counter = 0;
+    notifyListeners();
+  }
 }
 
 class MainApp extends StatelessWidget {
@@ -64,6 +90,111 @@ class MainApp extends StatelessWidget {
         ),
         home: Homepage(),
         // home: ProductCardDetailsPage(product: products[0]),
+      ),
+    );
+  }
+}
+
+class NewApp extends StatelessWidget {
+  const NewApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          titleMedium: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          bodySmall: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+          titleLarge: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+
+      title: 'Counter App',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Counter App"),
+      ),
+      body: Center(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Counter App",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              const SizedBox(height: 20),
+              Consumer<CounterProvider>(
+                builder: (context, value, child) => Text(
+                  value.counter.toString(),
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      context
+                          .read<CounterProvider>()
+                          .increment();
+                    },
+                    icon: const Icon(Icons.add),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      context
+                          .read<CounterProvider>()
+                          .decrement();
+                    },
+                    icon: const Icon(Icons.remove),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          int value = context.read<CounterProvider>().counter;
+          if (value != 0) {
+            context.read<CounterProvider>().reset();
+            return;
+          }
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Value has been reset")),
+          );
+        },
+        child: const Text("Reset"),
       ),
     );
   }
