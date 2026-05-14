2164f05570156fc6ee77a94914b1a4c2108b7a09
Emmanuel Joshua <emiranojoshua@gmail.com>
2026-05-14 09:01:03 +0100

    Enhance NoteService: add stream controller for notes, improve note management methods, and handle user creation

--- lib/services/auth/note_service.dart
+++ lib/services/auth/note_service.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:ecommerce_app/services/crud/crud_exceptions.dart';
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
@@ -7,9 +9,27 @@ import 'package:path_provider/path_provider.dart'
         MissingPlatformDirectoryException;
 import 'package:path/path.dart' show join;
 
-
 class NoteService {
   Database? _db;
+
+  List<DatabaseNote> _notes = [];
+
+  final _notesStreamController =
+      StreamController<List<DatabaseNote>>.broadcast();
+
+  Future<DatabaseUsers> getOrCreateUser({
+    required String email,
+  }) async {
+    try {
+      final user = await getUser(email: email);
+      return user;
+    } on CouldNotFindUserException {
+      final createdUser = await createUser(email: email);
+      return createdUser;
+    } catch (e) {
+      rethrow;
+    }
+  }
 
   Future<DatabaseNote> updateNote({
     required DatabaseNote note,
@@ -26,7 +46,11 @@ class NoteService {
       throw CouldNotUpdateNoteException();
     }
     {
-      return getNote(id: note.id);
+      final updatedNote = await getNote(id: note.id);
+      _notes.removeWhere((note) => note.id == updatedNote.id);
+      _notes.add(updatedNote);
+      _notesStreamController.add(_notes);
+      return updatedNote;
     }
   }
 
@@ -47,13 +71,20 @@ class NoteService {
     if (notes.isEmpty) {
       throw CouldNotFindNoteException();
     } else {
-      return DatabaseNote.fromMap(notes.first);
+      final note = DatabaseNote.fromMap(notes.first);
+      _notes.removeWhere((note) => note.id == id);
+      _notes.add(note);
+      _notesStreamController.add(_notes);
+      return note;
     }
   }
 
   Future<int> deleteAllNotes() async {
     final db = _getDatabaseOrThrow();
-    return await db.delete(noteTable);
+    final numberDeleted = await db.delete(noteTable);
+    _notes.clear();
+    _notesStreamController.add(_notes);
+    return numberDeleted;
   }
 
   Future<void> deleteNote({required int id}) async {
@@ -66,6 +97,12 @@ class NoteService {
 
     if (deletedCount == 0) {
       throw CouldNotDeleteNoteException();
+    } else {
+      final countBefore = _notes.length;
+      _notes.removeWhere((note) => note.id == id);
+      if (_notes.length != countBefore) {
+        _notesStreamController.add(_notes);
+      }
     }
   }
 
@@ -94,7 +131,8 @@ class NoteService {
       text: text,
       isSyncyedWithCloud: true,
     );
-
+    _notes.add(note);
+    _notesStreamController.add(_notes);
     return note;
   }
 
